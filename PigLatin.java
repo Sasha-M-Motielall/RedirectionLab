eac80a242b8b89192df4d7ecb9c9ba9031003287
Sasha-M-Motielall <smotielall20@stuy.edu>
2021-01-05 22:43:28 -0500

    pigLatinSimple, pigLatin, pigLatinBest, tested

--- PigLatin.java
+++ PigLatin.java
@@ -55,4 +55,26 @@ public class PigLatin{
     if(weird(d, str)){return str + "hay" + punc;}
     return str.substring(1) + str.charAt(0) + "ay" + punc;
   }
+
+  public static void main(String[] args){
+    System.out.println(pigLatinSimple("mock"));
+    System.out.println(pigLatinSimple("pie"));
+    System.out.println(pigLatinSimple("david"));
+    System.out.println(pigLatinSimple("aaron"));
+
+    System.out.println(pigLatin("the"));
+    System.out.println(pigLatin("check"));
+    System.out.println(pigLatin("skee"));
+    System.out.println(pigLatin("emu"));
+    System.out.println(pigLatin("grade"));
+
+    System.out.println(pigLatinBest("*emu"));
+    System.out.println(pigLatinBest("4chan"));
+    System.out.println(pigLatinBest("fish!"));
+    System.out.println(pigLatinBest("fish"));
+    System.out.println(pigLatinBest("the."));
+    System.out.println(pigLatinBest("cat!"));
+    System.out.println(pigLatinBest("amazing?"));
+    System.out.println(pigLatinBest("apple%"));
+  }
 }
