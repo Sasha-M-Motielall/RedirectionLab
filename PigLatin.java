bffe178badeb6edf02bc3d39d7485080e29b2fe5
Sasha-M-Motielall <smotielall20@stuy.edu>
2021-01-05 22:01:26 -0500

    pigLatinSimple and pigLatin created

--- PigLatin.java
+++ PigLatin.java
@@ -18,4 +18,24 @@ public class PigLatin{
     }
     return statement;
   }
+
+  public static String pigLatinSimple(String s){
+   String fix = s.toLowerCase();
+   char[] a = new char[]{'a','e','i','o','u'};
+   if(weird(a, fix)){return fix + "hay";}
+   return fix.substring(1) + fix.charAt(0) + "ay";
+  }
+
+  public static String pigLatin(String st){
+    st = st.toLowerCase();
+    char[] b = new char[]{'a','e','i','o','u'};
+    String[] c = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl",
+                              "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr",
+                              "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp",
+                              "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    if(st.length()<=1){return st + "ay";}
+    if(weirder(c, st)){return st.substring(2) + st.substring(0,2) + "ay";}
+    if(weird(b, st)){return st + "hay";}
+    return st.substring(1) + st.charAt(0) + "ay";
+  }
 }
